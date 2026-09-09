91be01d367ada679bcd045e243ef44375c4a7215
amcallservices <amcallservices@gmail.com>
2026-09-10 00:31:05 +0100

    Add Trustpilot meta tag verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,5 +12,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="it">{children}</html>;
+  return <html lang="it"><head><meta name="trustpilot-one-time-domain-verification-id" content="467bdb4a-bba4-4057-8eab-32cc5011f334" /></head>{children}</html>;
 }
